4691f427a7343946d1ab913c6b882bed045053bd
Graham Smith <graham.smith+staging@dbtlabs.com>
2024-07-26 17:53:03 +0000

    fix up model

--- models/block_stats.sql
+++ models/block_stats.sql
@@ -2,24 +2,26 @@
 
 with average_market_value as (
   select block_id, percentile_cont(0.5) within group (order by market_value) as median,
- avg(market_value) as mean
+  round(avg(market_value)) as mean,
+  count(*) as properties
   from properties_by_block
   group by block_id
 ), average_violent_crime as (
   select block_id, count(*) as incidents
   from crimes_by_block
-  where ucr_general < 500
+  where cast(ucr_general as int) < 500
   group by block_id
 ), average_nonviolent_crime as (
   select block_id, count(*) as incidents
   from crimes_by_block
-  where ucr_general >= 500
+  where cast(ucr_general as int) >= 500
   group by block_id
 )
 select
-  block_id,
+  amv.block_id,
   median as median_value,
   mean as mean_value,
+  properties,
   avc.incidents as violent_incidents,
   anc.incidents as nonviolent_incidents
 from average_market_value amv
